4820d48d766aa01185ffcddc968bbab2e1893800
vnsiuhtiwue <87399427+vnsiuhtiwue@users.noreply.github.com>
2022-10-03 09:38:23 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,10 +3,10 @@
 # configs
 AUUID=aff8df54-b326-40d9-a5ee-e798cdf57e24
 CADDYIndexPage=https://github.com/ripienaar/free-for-dev/archive/master.zip
-CONFIGCADDY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/Caddyfile
-CONFIGXRAY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/xray.json
+CONFIGCADDY=https://raw.githubusercontent.com/vnsiuhtiwue/hqdrwxm/master/etc/Caddyfile
+CONFIGXRAY=https://raw.githubusercontent.com/vnsiuhtiwue/hqdrwxm/master/etc/xray.json
 ParameterSSENCYPT=chacha20-ietf-poly1305
-StoreFiles=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/StoreFiles
+StoreFiles=https://raw.githubusercontent.com/vnsiuhtiwue/hqdrwxm/master/etc/StoreFiles
 #PORT=4433
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
